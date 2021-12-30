d93f7ac1a25c48b96e1f967bb885b18c89fe801e
Takuma Kira <takuma@kirakiraworx.com>
2021-12-30 23:02:38 +0900

    [refactor] Add style test for Title component

--- src/stories/Title.spec.jsx
+++ src/stories/Title.spec.jsx
@@ -1,10 +1,17 @@
 import { render, screen } from '@testing-library/react';
-import Title from './Title';
+import 'jest-styled-components';
+import renderer from 'react-test-renderer';
 import config from '../config.json';
+import Title from './Title';
 
 it(`should render title with H1 tag`, () => {
   render(<Title />);
   const titleElem = screen.getByText(config.title);
   expect(titleElem).toBeInTheDocument();
   expect(titleElem.tagName).toBe('H1');
+});
+
+it(`should have distinct color`, () => {
+  const titleElem = renderer.create(<Title />).toJSON()
+  expect(titleElem).toHaveStyleRule('color', '#FFFFFF')
 });
